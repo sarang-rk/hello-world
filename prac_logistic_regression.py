d4212ae5efd0ff84cd70e3930e7e529e69e14c60
sarang-rk <40624552+sarang-rk@users.noreply.github.com>
2018-08-27 15:46:24 +0530

    Add files via upload

---
+++ prac_logistic_regression.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 31 20:34:02 2018
+
+@author: sk186089
+"""
+
+import numpy as np
+import matplotlib as mp
+import pandas as pd
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+#object classifier from class LogisticRegression
+#command i for help
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+
+
+
+
+
+
